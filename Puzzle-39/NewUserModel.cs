614a403e077ceec767f82627342face01400a58d
Carl Franklin <carl@appvnext.com>
2024-06-18 20:50:38 -0400

    Add project files.

---
+++ Puzzle-39/NewUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Puzzle_39;
+
+public class NewUserModel
+{
+
+    [Required]
+    public string? UserName { get; set; } = string.Empty;
+
+    [Required]
+    public string? Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string? Email { get; set; } = string.Empty;
+
+}
